a77d1046faee7059a3c1a5b85d9e70050d29dab0
chereshenka <55566045+chereshenka@users.noreply.github.com>
2022-10-26 15:22:33 +0300

    Update app.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,109 +1,116 @@
 import { Component } from "react";
-import PropTypes from "prop-types";
-import { formatDistanceToNow } from "date-fns";
 
-export default class Task extends Component {
-  static propTypes = {
-    description: PropTypes.string,
-    completed: PropTypes.bool,
-    date: PropTypes.number,
-    onLabel: PropTypes.func,
-    onDelete: PropTypes.func,
-  };
+import Header from "../header";
+import TaskList from "../task-list";
+import Footer from "../footer";
 
-  static defaultProps = {
-    date: "5 min ago",
-  };
-
-  state = {
-    min: null,
-    sec: null,
-    timer: null,
-  };
-  componentDidMount() {
-    localStorage.setItem(
-      this.props.partNum,
-      this.props.min * 60 + this.props.sec
-    );
-    this.degreeseTimer();
+export default class App extends Component {
+  constructor() {
+    super();
+    this.maxId = 100;
+    this.state = {
+      todoData: [
+        this.createTodoItem("Drink Coffee"),
+        this.createTodoItem("Make App"),
+        this.createTodoItem("Have a lunch"),
+      ],
+      filter: "all",
+    };
   }
 
-  componentWillUnmount() {
-    localStorage.removeItem(this.props.partNum);
-    clearInterval(this.interval);
+  createTodoItem(description, min, sec) {
+    return {
+      description,
+      completed: false,
+      partNum: this.maxId++,
+      date: Date.now(),
+      min: +min || 0,
+      sec: +sec || 0,
+    };
   }
 
-  degreeseTimer = () => {
-    const localTime = localStorage.getItem(this.props.partNum);
-    let sec = localTime % 60;
-    let min = Math.floor(localTime / 60);
-    this.setState({
-      min,
-      sec,
+  toggleProperty = (arr, id, propName) => {
+    const idx = arr.findIndex((el) => el.partNum === id);
+    const oldItem = arr[idx];
+    const newItem = { ...oldItem, [propName]: !oldItem[propName] };
+    return [...arr.slice(0, idx), newItem, ...arr.slice(idx + 1)];
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, "completed"),
+      };
     });
   };
 
-  startTaskTimer = () => {
-    let localTime = localStorage.getItem(this.props.partNum);
-    if (localTime > 0) {
-      localStorage.setItem(this.props.partNum, --localTime);
-      this.degreeseTimer();
-    }
+  addItem = (text, min, sec) => {
+    console.log("active");
+    const newItem = this.createTodoItem(text, min, sec);
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+
+      return {
+        todoData: newArr,
+      };
+    });
   };
-  pauseTaskTimer = (e) => {
-    let button = e.target;
-    if (button.className === "icon-timer icon-pause") {
-      console.log("pause");
-      clearInterval(this.state.timer);
-      this.setState({ timer: "" });
-    }
-    if (button.className === "icon-timer icon-play") {
-      console.log("start");
-      if (!this.state.timer) {
-        this.setState({ timer: setInterval(this.startTaskTimer, 1000) });
-        this.state.timer;
-      }
-    }
+
+  clearCompleted = () => {
+    this.setState(({ todoData }) => {
+      const cleanArr = todoData.filter((el) => !el.completed);
+      return { todoData: cleanArr };
+    });
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return { todoData: newArr };
+    });
+  };
+
+  filterSetState = (value) => {
+    this.setState({ filter: value });
   };
 
   render() {
-    const { description, completed, date, onLabel, onDelete } = this.props;
-    const id = this.props.partNum;
+    const { todoData, filter } = this.state;
+    const taskNotCompletedCount = this.state.todoData.filter(
+      (el) => !el.completed
+    ).length;
+    console.log(todoData);
     return (
-      <div className="view">
-        <input
-          className="toggle"
-          type="checkbox"
-          onChange={onLabel}
-          checked={completed}
-        />
-        <label>
-          <span className="description">{description}</span>
-          <span className="timer-buttons">
-            <button
-              id={id}
-              className="icon-timer icon-play"
-              onClick={this.pauseTaskTimer}
-            ></button>
-            <button
-              id={id}
-              className="icon-timer icon-pause"
-              onClick={this.pauseTaskTimer}
-            ></button>
-            <span className="time">
-              {this.state.min}:{this.state.sec}
-            </span>
-          </span>
-          <span className="created">
-            {formatDistanceToNow(date, {
-              includeSeconds: true,
-              addSuffix: true,
+      <section className="todoapp">
+        <Header onItemAdd={this.addItem} setTimer={this.setUserTime} />
+        <section className="main">
+          <TaskList
+            todos={todoData.filter((el) => {
+              if (filter === "active") {
+                return !el.completed;
+              }
+              if (filter === "all") {
+                return el;
+              }
+              if (filter === "completed") {
+                return el.completed;
+              }
+              return el;
             })}
-          </span>
-        </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy" onClick={onDelete}></button>
-      </div>
+            onDelete={this.deleteItem}
+            onToggle={this.onToggleDone}
+          />
+        </section>
+        <Footer
+          onFilter={this.filterSetState}
+          taskToComplete={taskNotCompletedCount}
+          clearAll={this.clearCompleted}
+        />
+        {/* <button >button</button> */}
+      </section>
     );
   }
 }
